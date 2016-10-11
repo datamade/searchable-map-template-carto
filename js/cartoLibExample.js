82e1f14bad7f6b82cc12693befb8b868d5a46ff2
reginafcompton <reginafcompton@gmail.com>
2016-10-11 16:33:40 -0500

    Create custom hover text

--- js/cartoLibExample.js
+++ js/cartoLibExample.js
@@ -3,7 +3,7 @@ $(function() {
   var exMap = new CartoLib
   exMap.setTableName('chicago_libraries_2016');
   exMap.setUserName('datamade');
-  // exMap.setFields('pin');
+  exMap.setFields('full_address, name, hours_of_operation, phone');
   exMap.setMapDivName('mapCanvas');
   exMap.setDefaultZoom(11);
   exMap.setCentroid(41.901557, -87.630360)
@@ -17,17 +17,18 @@ $(function() {
 
         layerZero = layer.getSubLayer(0);
         layerZero.setInteraction(true);
-
-        layerZero.on('featureOver', function(data) {
+        layerZero.on('featureOver', function(e, latlng, pos, data, subLayerIndex) {
           $(mapName).css('cursor','pointer');
-          CartoLib.prototype.updateInfoBox("Text about a location", "infoBox");
+          // Add custom text to the info window.
+          var text = makeInfoText(data);
+          CartoLib.prototype.updateInfoBox(text, "infoBox");
         });
         layerZero.on('featureOut', function() {
           $(mapName).css('cursor','inherit');
           CartoLib.prototype.clearInfoBox("infoBox");
         });
         layerZero.on('featureClick', function(data){
-          // Add something here, e.g., a modal window.
+          // You can add something here, e.g., a modal window.
         });
       }).error(function(e) {
         console.log(e)
@@ -38,4 +39,16 @@ $(function() {
         exMap.doSearch();
       });
 });
+
+// Build this custom function yourself. It should format data from your Carto map into HTML.
+function makeInfoText(data) {
+  var name = "<h4>" + data.name + "</h4>"
+  var address = "<p><i class='fa fa-map-marker' aria-hidden='true'></i> " + data.full_address + "</p>"
+  var hours = "<p><i class='fa fa-calendar' aria-hidden='true'></i> " + data.hours_of_operation + "</p>"
+  var phone = "<p><i class='fa fa-phone' aria-hidden='true'></i> " + data.phone + "</p>"
+  var html = name + address + hours + phone
+
+  return html
+};
+
 
