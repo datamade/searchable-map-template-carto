449ed140dcfed588dac1337dd3a7cd325c9dc082
reginafcompton <reginafcompton@gmail.com>
2016-10-12 11:52:49 -0500

    Refactor for readability

--- js/cartoLib.js
+++ js/cartoLib.js
@@ -2,65 +2,39 @@
 CartoLib = (function() {
 // Declaration of CartoLib function.
   function CartoLib() {
-    this._mapSettings = {
-      cartoTableName: '',
-      cartoUserName: '',
-      locationScope: 'chicago',
-      mapDivName: '',
-      map: null,
-      mapCentroid: new L.LatLng(41.901557, -87.630360),
-      defaultZoom: 11,
-      lastClickedLayer: null,
-      geojson: null,
-      fields: '',
-      currentPinpoint: '',
-    };
-
+    // Quick variable reference to map settings.
+    this.cartoTableName   = '';
+    this.cartoUserName    = '';
+    this.locationScope    = 'chicago';
+    this.mapDivName       = '';
+    this.map              = null;
+    this.mapCentroid      = new L.LatLng(41.901557, -87.630360),
+    this.defaultZoom      = 11;
+    this.lastClickedLayer = null;
+    this.geojson          = null;
+    this.fields           = '';
+    this.currentPinpoint  = '';
+    this.centerMark       = '';
+    this.radiusCircle     = '';
+    // Create geocoder object to access Google Maps API. Add underscore to insure variable safety.
     this._geocoder = new google.maps.Geocoder();
-    this._centerMark = '';
-    this._radiusCircle = '';
-
-    // Functions to update settings.
-    this.setTableName = function(name) {
-      this._mapSettings.cartoTableName = name;
-    };
-
-    this.setUserName = function(name) {
-      this._mapSettings.cartoUserName = name;
-    };
-
-    this.setFields = function(fields) {
-      this._mapSettings.fields = fields;
-    };
-
-    this.setMapDivName = function(name) {
-      this._mapSettings.mapDivName = name;
-    };
-
-    this.setCentroid = function(latitude, longitude) {
-      this._mapSettings.mapCentroid = new L.LatLng(latitude, longitude);
-    };
-
-    this.setDefaultZoom = function(zoom) {
-      this._mapSettings.defaultZoom = zoom;
-    };
-
   }
 
+  // Give CartoLib its behaviors.
   CartoLib.prototype.initiateMap = function() {
       // Initiate leaflet map
-      var div = this._mapSettings.mapDivName;
+      var div = this.mapDivName;
       // var geocoder = new google.maps.Geocoder();
       var layer = new L.Google('ROADMAP');
 
-      this._mapSettings.map = new L.Map('mapCanvas', {
-        center: this._mapSettings.mapCentroid,
-        zoom: this._mapSettings.defaultZoom,
+      this.map = new L.Map('mapCanvas', {
+        center: this.mapCentroid,
+        zoom: this.defaultZoom,
         scrollWheelZoom: false,
         tapTolerance: 30
       });
 
-      this._mapSettings.map.addLayer(layer);
+      this.map.addLayer(layer);
   }
 
   CartoLib.prototype.addInfoBox = function(mapPosition, divName, text) {
@@ -73,7 +47,7 @@ CartoLib = (function() {
         return this._div;
       };
 
-      info.addTo(this._mapSettings.map);
+      info.addTo(this.map);
   }
 
   CartoLib.prototype.updateInfoBox = function(infoText, divName) {
@@ -92,13 +66,13 @@ CartoLib = (function() {
     }
 
     var layerOpts = {
-      user_name: this._mapSettings.cartoUserName,
+      user_name: this.cartoUserName,
       type: 'cartodb',
       cartodb_logo: false,
       sublayers: sublayerArr
     }
 
-    var createdLayer = cartodb.createLayer(this._mapSettings.map, layerOpts, { https: true });
+    var createdLayer = cartodb.createLayer(this.map, layerOpts, { https: true });
     return createdLayer;
   }
 
@@ -107,7 +81,7 @@ CartoLib = (function() {
     var layer = {
       sql: sqlQuery,
       cartocss: $(cartoCSSId).html().trim(),
-      interactivity: this._mapSettings.fields
+      interactivity: this.fields
     }
 
     return layer
@@ -122,7 +96,7 @@ CartoLib = (function() {
     else if (radius >= 400) zoom = 16; // 1/4 mile
     else zoom = 16;
 
-    this._mapSettings.map.setView(new L.LatLng( this._mapSettings.currentPinpoint[0], this._mapSettings.currentPinpoint[1] ), zoom)
+    this.map.setView(new L.LatLng( this.currentPinpoint[0], this.currentPinpoint[1] ), zoom)
   }
 
   CartoLib.prototype.doSearch = function() {
@@ -132,7 +106,7 @@ CartoLib = (function() {
     // #search-address refers to a div id in map-example.html. You can rename this div.
     var address = $("#search-address").val();
     var radius = $("#search-radius").val();
-    var location = this._mapSettings.locationScope;
+    var location = this.locationScope;
 
     if (radius == null && address != "") {
       radius = 8050;
@@ -141,8 +115,8 @@ CartoLib = (function() {
     if (address != "") {
       this._geocoder.geocode( { 'address': address }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-         cartoLib._mapSettings.currentPinpoint = [results[0].geometry.location.lat(), results[0].geometry.location.lng()];
-          var geoSearch = "ST_DWithin(ST_SetSRID(ST_POINT(" + cartoLib._mapSettings.currentPinpoint[1] + ", " + cartoLib._mapSettings.currentPinpoint[0] + "), 4326)::geography, the_geom::geography, " + radius + ")";
+         cartoLib.currentPinpoint = [results[0].geometry.location.lat(), results[0].geometry.location.lng()];
+          var geoSearch = "ST_DWithin(ST_SetSRID(ST_POINT(" + cartoLib.currentPinpoint[1] + ", " + cartoLib.currentPinpoint[0] + "), 4326)::geography, the_geom::geography, " + radius + ")";
           var whereClause = " WHERE the_geom is not null AND " + geoSearch;
 
           cartoLib.setZoom(radius);
@@ -157,7 +131,7 @@ CartoLib = (function() {
   }
 
   CartoLib.prototype.addIcon = function() {
-    this._centerMark = new L.Marker(this._mapSettings.currentPinpoint, {
+    this._centerMark = new L.Marker(this.currentPinpoint, {
       icon: (new L.Icon({
         iconUrl: 'push_pin.png',
         iconSize: [30, 30],
@@ -166,25 +140,25 @@ CartoLib = (function() {
       )
     });
 
-    this._centerMark.addTo(this._mapSettings.map);
+    this._centerMark.addTo(this.map);
   }
 
   CartoLib.prototype.addCircle = function(radius) {
-    this._radiusCircle = new L.circle(this._mapSettings.currentPinpoint, radius, {
+    this._radiusCircle = new L.circle(this.currentPinpoint, radius, {
         fillColor:'#1d5492',
         fillOpacity:'0.2',
         stroke: false,
         clickable: false
     });
 
-    this._radiusCircle.addTo(this._mapSettings.map);
+    this._radiusCircle.addTo(this.map);
   }
 
   CartoLib.prototype.clearSearch = function() {
     if (this._centerMark)
-      this._mapSettings.map.removeLayer( this._centerMark );
+      this.map.removeLayer( this._centerMark );
     if (this._radiusCircle)
-      this._mapSettings.map.removeLayer( this._radiusCircle );
+      this.map.removeLayer( this._radiusCircle );
   }
 
   return CartoLib;
