cb4b20b74b8c781ba59ab1ed9da50035d3d4b20d
reginafcompton <reginafcompton@gmail.com>
2017-03-23 10:00:02 -0500

    Something....?

--- js/cartoLibExample.js
+++ js/cartoLibExample.js
@@ -4,12 +4,12 @@ $(function() {
   // Update map settings.
   exMap.cartoTableName  = 'chicago_libraries_2016';
   exMap.cartoUserName   = 'datamade';
-  exMap.fields          = 'full_address, name, hours_of_operation, phone';
+  exMap.fields          = 'park_name';
   exMap.mapDivName      = 'mapCanvas';
   // Create a map!
   exMap.initiateMap()
   exMap.addInfoBox('bottomright', 'infoBox');
-  var layer1 = exMap.defineSublayer("select * from chicago_libraries_2016", '#carto-result-style');
+  var layer1 = exMap.defineSublayer("select * from parks_public_art", '#carto-result-style');
   // Custom code.
   exMap.createCartoLayer(layer1).addTo(exMap.map)
       .done(function(layer) {
@@ -42,13 +42,13 @@ $(function() {
 
 // Build this custom function yourself. It should format data from your Carto map into HTML.
 function makeInfoText(data) {
-  var name = "<h4>" + data.name + "</h4>"
-  var address = "<p><i class='fa fa-map-marker' aria-hidden='true'></i> " + data.full_address + "</p>"
-  var hours = "<p><i class='fa fa-calendar' aria-hidden='true'></i> " + data.hours_of_operation + "</p>"
-  var phone = "<p><i class='fa fa-phone' aria-hidden='true'></i> " + data.phone + "</p>"
-  var html = name + address + hours + phone
+  // var name = "<h4>" + data.name + "</h4>"
+  // var address = "<p><i class='fa fa-map-marker' aria-hidden='true'></i> " + data.full_address + "</p>"
+  // var hours = "<p><i class='fa fa-calendar' aria-hidden='true'></i> " + data.hours_of_operation + "</p>"
+  // var phone = "<p><i class='fa fa-phone' aria-hidden='true'></i> " + data.phone + "</p>"
+  // var html = name + address + hours + phone
 
-  return html
+  // return html
 };
 
 // Build this custom funciton yourself. It will outline a parcel on the map, when clicked.
@@ -58,18 +58,18 @@ function getOneParcel(full_address, map) {
       // }
       var address = String(full_address)
       var sql = new cartodb.SQL({user: 'datamade', format: 'geojson'});
-      console.log(sql)
-      console.log(full_address)
-      sql.execute("select * from chicago_libraries_2016 where full_address='" + address + "'")
+      sql.execute("select * from parks_public_art where full_address='" + address + "'")
         .done(function(data){
             var shape = data.features[0];
             console.log(shape)
-            // CartoLib.lastClickedLayer = L.geoJson(shape);
-            L.geoJson(shape).addTo(map);
-            // CartoLib.lastClickedLayer.addTo(map);
+            CartoLib.lastClickedLayer = L.geoJson(shape);
+            // L.geoJson(shape).addTo(map);
+            console.log(L.geoJson(shape))
+
+            CartoLib.lastClickedLayer.addTo(CartoLib.map);
             // CartoLib.lastClickedLayer.setStyle({fillColor:'#f7fcb9', weight: 2, fillOpacity: 1, color: '#000'});
-            // LargeLots.map.setView(LargeLots.lastClickedLayer.getBounds().getCenter(), 17);
-      //       LargeLots.selectParcel(shape.properties);
+            // CartoLib.map.setView(CartoLib.lastClickedLayer.getBounds().getCenter(), 17);
+            // LargeLots.selectParcel(shape.properties);
         }).error(function(e){console.log(e)});
       // window.location.hash = 'browse';
 
